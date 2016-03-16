f3a6b12f8c3b80a2ec49becb815ceb4c1d80cb28
Slaventiy777 <zawsv777@gmail.com>
2016-03-16 08:35:09 +0200

    Новые изменения
    Файл обмена теперь создается при подключении, его формат изменен.
    Создана регистрация и хранение зарегистрированных пользователей

--- backgammon/Server/src/ua/sumdu/java2ee/mikhailishinNikolay/client/TestClient.java
+++ backgammon/Server/src/ua/sumdu/java2ee/mikhailishinNikolay/client/TestClient.java
@@ -4,6 +4,9 @@ package ua.sumdu.java2ee.mikhailishinNikolay.client;
 import ua.sumdu.java2ee.mikhailishinNikolay.transport.ClientTransport;
 import ua.sumdu.java2ee.mikhailishinNikolay.transport.Transport;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -20,7 +23,16 @@ public class TestClient {
         Socket clientSocket = new Socket("localhost", 1024);
         System.out.println("Registering...");
         ClientTransport clientTransport = new ClientTransport(clientSocket);
-        clientTransport.send();
+        try {
+            File regFile = clientTransport.createRegistrationFile("player2", "123");
+            clientTransport.send(regFile);
+        } catch (ParserConfigurationException e) {
+            System.err.println("File creation error");
+        } catch (TransformerException e) {
+            System.err.println("File creation error");
+        }
+
+
     }
 
 }
